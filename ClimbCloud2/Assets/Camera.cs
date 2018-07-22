219cb88954546e91e644288e66b41e40836b56b5
Eun Ju Jhong <eun01300714@gmail.com>
2018-07-23 08:42:51 +0900

    This is ClimbCloud Game

---
+++ ClimbCloud2/Assets/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    GameObject palyer;
+
+	// Use this for initialization
+	void Start () {
+        this.palyer = GameObject.Find("cat");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = this.palyer.transform.position;
+        transform.position = new Vector3(
+            transform.position.x, playerPos.y, transform.position.z);
+	}
+}
---
+++ ClimbCloud2/Assets/clear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // LoadScene을 사용할 때 중요!!
+
+public class clear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene("GameScene");
+	}
+}
